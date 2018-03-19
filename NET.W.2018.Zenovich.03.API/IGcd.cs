3c3df4f3cdb1664252b3fe0968109ca62a3ffb76
azenovich <alexander.zenovich@gmail.com>
2018-03-19 15:39:23 +0300

    StopWatch was added in Gcd class. And IGcd was changed

--- NET.W.2018.Zenovich.03.API/IGcd.cs
+++ NET.W.2018.Zenovich.03.API/IGcd.cs
@@ -9,5 +9,6 @@ namespace NET.W._2018.Zenovich._03.API
     public interface IGcd
     {
         int CalculateGcd(params int[] numbers);
+        TimeSpan CompletionTime { get; }
     }
 }
--- NET.W.2018.Zenovich.03.Model/Gcd.cs
+++ NET.W.2018.Zenovich.03.Model/Gcd.cs
@@ -1,6 +1,7 @@
 ﻿using NET.W._2018.Zenovich._03.API;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,39 @@ namespace NET.W._2018.Zenovich._03.Model
 {
     public class Gcd : IGcd
     {
+        private Stopwatch _stopwatch;
+        private TimeSpan _timeSpan;
+
+        public TimeSpan CompletionTime
+        {
+            get
+            {
+                if (_timeSpan == null)
+                {
+                    throw new InvalidOperationException("Method calculateGcd should be called");
+                }
+
+                return _timeSpan;
+            }
+            protected set
+            {
+                _timeSpan = value;
+            }
+        }
+
+        protected Stopwatch Stopwatch
+        {
+            get
+            {
+                if (_stopwatch == null)
+                {
+                    _stopwatch = new Stopwatch();
+                }
+
+                return _stopwatch;
+            }
+        }
+
         private int CalculateGcd(int left, int right)
         {
             int temp;
@@ -22,7 +56,7 @@ namespace NET.W._2018.Zenovich._03.Model
             return Math.Abs(left);
         }
 
-        public int CalculateGcd(params int[] numbers)
+        public int MainGcd(params int[] numbers)
         {
             int length, result;
 
@@ -43,6 +77,28 @@ namespace NET.W._2018.Zenovich._03.Model
             for (int i = 2; i < length; i++)
             {
                 result = CalculateGcd(numbers[i], result);
+            }
+
+            return result;
+        }
+
+        public int CalculateGcd(params int[] numbers)
+        {
+            int result;
+            Stopwatch.Start();
+            try
+            {
+                result = MainGcd(numbers);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+            finally
+            {
+                Stopwatch.Stop();
+                CompletionTime = Stopwatch.Elapsed;
+                Stopwatch.Reset();
             }
 
             return result;
