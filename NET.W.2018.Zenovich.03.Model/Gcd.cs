135e9baefefd64a92e5338493579a36298846bdf
azenovich <alexander.zenovich@gmail.com>
2018-03-19 15:07:02 +0300

    Добавьте файлы проекта.

---
+++ NET.W.2018.Zenovich.03.Model/Gcd.cs
@@ -0,0 +1,51 @@
+﻿using NET.W._2018.Zenovich._03.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2018.Zenovich._03.Model
+{
+    public class Gcd : IGcd
+    {
+        private int CalculateGcd(int left, int right)
+        {
+            int temp;
+            while (right != 0)
+            {
+                temp = left % right;
+                left = right;
+                right = temp;
+            }
+
+            return Math.Abs(left);
+        }
+
+        public int CalculateGcd(params int[] numbers)
+        {
+            int length, result;
+
+            if (numbers == null)
+            {
+                throw new ArgumentNullException(nameof(numbers));
+            }
+
+            length = numbers.Length;
+
+            if (length < 2)
+            {
+                throw new ArgumentException("Count of numbers should be more than 2");
+            }
+
+            result = CalculateGcd(numbers[0], numbers[1]);
+
+            for (int i = 2; i < length; i++)
+            {
+                result = CalculateGcd(numbers[i], result);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NET.W.2018.Zenovich.03.Tests/GcdTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NET.W._2018.Zenovich._03.API;
+using NET.W._2018.Zenovich._03.Model;
+
+namespace NET.W._2018.Zenovich._03.Tests
+{
+    [TestClass]
+    public class GcdTest
+    {
+        public TestContext TestContext { get; set; }
+        private static IGcd gcd;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            gcd = new Gcd();
+        }
+
+        [TestMethod]
+        [DeploymentItem("Files\\numbersGcd.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+        "numbersGcd.xml",
+        "Number",
+        DataAccessMethod.Sequential)]
+        public void CalculateGcd_NumbersFromXml_ExpectedValuesFromXml()
+        {
+            // arrange
+            string[] numbersXml = Convert.ToString(TestContext.DataRow["numbers"]).Split(',');
+            int length = numbersXml.Length;
+
+            int[] numbers = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i] = Convert.ToInt32(numbersXml[i]);
+            }
+            int expected = Convert.ToInt32(TestContext.DataRow["expected"]);
+
+            // act
+            int actual = gcd.CalculateGcd(numbers);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
